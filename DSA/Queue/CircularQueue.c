2edf53083cc22744fadb86601944c03b648f5bde
Rohit Roy <creative.roy2003@gmail.com>
2022-07-17 23:05:23 +0530

    clearing circular queue operation added more comments added - function purpose defined

--- DSA/Queue/CircularQueue.c
+++ DSA/Queue/CircularQueue.c
@@ -4,7 +4,7 @@
 
 int arr[SIZE], front = -1, rear = -1;
 
-void choice()
+void choice() // menu driven choice function to perform queue operations via user choice inputs
 {
     int ch, val;
     int isFull();
@@ -15,7 +15,8 @@ void choice()
     printf("\n1: Insert elements");
     printf("\n2: Delete elements");
     printf("\n3: Display queue elements");
-    printf("\n4: Exit");
+    printf("\n4: Clear queue");
+    printf("\n5: Exit");
     printf("\nEnter your choice: ");
     scanf("%d", &ch);
     switch (ch)
@@ -35,15 +36,20 @@ void choice()
         break;
 
     case 4:
+        front = rear = -1;
+        printf("\nQueue is clear now!");
+        break;
+
+    case 5:
         exit(0);
         break;
 
     default:
         printf("\nInvalid choice entered.");
     }
-}
+} // end of choice()
 
-int isFull()
+int isFull() // function to check whether queue is full or not
 {
     if ((front == rear + 1 && front < SIZE) || (front == 0 && rear == SIZE - 1))
     {
@@ -52,14 +58,14 @@ int isFull()
     return 0;
 }
 
-int isEmpty()
+int isEmpty() // function to check whether the queue is empty or not
 {
     if (front == rear == -1)
         return 1; // implies queue is empty
     return 0;
 }
 
-void insert(int e)
+void insert(int e) // function o insert queue elements
 {
     if (isFull())
     {
@@ -71,9 +77,9 @@ void insert(int e)
     if (rear == SIZE - 1)
         rear = -1;
     arr[++rear] = e;
-}
+} // end of insert()
 
-void delete ()
+void delete () // function to delete queue elements from front end
 {
     int e;
     if (isEmpty())
@@ -89,10 +95,11 @@ void delete ()
         front = rear = -1;
     else
         front++;
-}
+} // end of delete()
 
-void display()
+void display() // displays queue elements
 {
+    // actually this function displays elements between front and rear index.
     int i;
     if (isEmpty())
     {
@@ -109,22 +116,23 @@ void display()
     }
     else
     {
-        for(i = front; i < SIZE; i++)
+        for (i = front; i < SIZE; i++)
         {
             printf("%d  ", arr[i]);
         }
-        for(i = 0; i <= rear; i++)
+        for (i = 0; i <= rear; i++)
         {
             printf("%d  ", arr[i]);
         }
     }
-}
+} // end of display()
 
 int main()
 {
+    // running the choice() until the program is terminated by the user (via choice option 5: Exit);
     while (1)
     {
         choice();
         printf("\n---------------------------------------------------------");
-    }   
-}
+    }
+} // end of main()
